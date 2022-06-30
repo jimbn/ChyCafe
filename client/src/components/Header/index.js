440211d2761dc4c77e3db0ce4bab75ad1332df3c
jimbn <nguyenb.jim@gmail.com>
2022-06-29 23:02:25 -0700

    add header

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Header = () => {
+  return(
+    <header>
+      <div>
+        <image>Icon</image>
+      </div>
+      <div>
+        {/* Nav */}
+      </div>
+      <div>
+        {/* Social Nav */}
+      </div>  
+    </header>
+  )
+}
+
+export default Header;
