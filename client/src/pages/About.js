f90fae82a7f7cb6346a86600f7e96ec7cbb1e6ea
jimbn <nguyenb.jim@gmail.com>
2022-06-30 21:45:58 -0700

    add about section

---
+++ client/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return(
+    <>
+    <div>
+      <h2>Hi there,</h2>
+      <h4>So you want to know a little about me?</h4>
+      <p>The name is Chynna (pronounced like China, the country). I am happily engaged to Nick, whom I've been with for over a decade. I am a Los Angeles native with a Bachelor's Degree in Communications. I'm also in the process of earning my Master's Degree.</p>
+      <p>I grew up visiting Disneyland with my family every year. Once I purchased a Disneyland Annual Pass, I began visiting Disneyland nearly every week. Disney Cruises are my obsession. Nick and I have been on 5 Disney Cruises so far and plan to go on our 6th one when the new ship, the Disney Wish, sets sail.</p>
+      <p>Because I love all things Disney, I have a passion for sharing tips, how-to's, DIYs, and whatever you can name that's related to the parks, resorts, entertainment, everything Disney!</p>
+    </div>
+    </>
+  )
+}
