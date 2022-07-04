64d828693d4cfdb4ecf92cabc2d803fdce48c085
jimbn <nguyenb.jim@gmail.com>
2022-07-03 19:19:24 -0700

    add contact info

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -4,6 +4,13 @@ const Contact = () => {
 
   return(
     <>
+    <div>
+      <h1>Contact Us</h1>
+      <p>hello@chynnamarie.com</p><br/>
+      <p>Mailbox:</p><br/>
+      <p>17216 Saticoy St #690</p>
+      <p>Van Nuys, CA 91406</p>
+    </div>
     </>
   )
 }
